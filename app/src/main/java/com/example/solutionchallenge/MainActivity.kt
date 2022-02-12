15c597da9bd1f8e1c94c2e91851b661524d75ef6
YoussefMaged766 <yoer766@gmail.com>
2022-02-12 02:35:01 +0200

    1

--- app/src/main/java/com/example/solutionchallenge/MainActivity.kt
+++ app/src/main/java/com/example/solutionchallenge/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //Moaaz 12/2
+        //aaloo
     }
 }
