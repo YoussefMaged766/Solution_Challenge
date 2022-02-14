3e5be8246c751b63188a858b137673ffba078940
YoussefMaged766 <yoer766@gmail.com>
2022-02-14 20:06:34 +0200

    adding two activity_intro_sliding.xml , activity_splash_screen.xml

---
+++ app/src/main/java/com/example/solutionchallenge/SplashScreen_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.solutionchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
