5aac44b30ae7589c6e9b97f3941f6b0fe29158a5
Moaaz <maaz1421@gmail.com>
2022-02-15 03:45:55 +0200

    Splash Activity

--- app/src/main/java/com/example/solutionchallenge/SplashScreen_Activity.kt
+++ app/src/main/java/com/example/solutionchallenge/SplashScreen_Activity.kt
@@ -1,20 +1,24 @@
 package com.example.solutionchallenge
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.HandlerCompat.postDelayed
+
 
 class SplashScreen_Activity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         Handler().postDelayed({
-            val intent = Intent(this, IntroSliding_Activity::class.java)
-            startActivity(intent)
             finish()
+            val intent = Intent(this, IntroSliding_Activity::class.java)
+                startActivity(intent)
+
+
         }, 3000)
     }
     }
