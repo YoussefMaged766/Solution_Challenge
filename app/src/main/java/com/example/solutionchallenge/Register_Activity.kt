52d903662cfa18fbd219f74b2491a27be3807a94
YoussefMaged766 <yoer766@gmail.com>
2022-02-19 23:07:51 +0200

    firebase

--- app/src/main/java/com/example/solutionchallenge/Register_Activity.kt
+++ app/src/main/java/com/example/solutionchallenge/Register_Activity.kt
@@ -3,17 +3,85 @@ package com.example.solutionchallenge
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class Register_Activity : AppCompatActivity() {
 
+
+
     lateinit var create_btn:Button
+    lateinit var txtemail :EditText
+    lateinit var txtpassword:EditText
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
+        val firebaseUser: FirebaseUser? = auth.currentUser
+        val userId= auth.uid
+
+
+
         create_btn = findViewById(R.id.btn_createAccount)
+        txtemail = findViewById(R.id.txt_email)
+        txtpassword = findViewById(R.id.txt_password)
+
+
+
+
         create_btn.setOnClickListener {
-            startActivity(Intent(this , UserProfile_Activity::class.java))
+
+            val userEmail  = txtemail.text.toString().trim()
+            val userPassword = txtpassword.text.toString().trim()
+
+            auth.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener{
+
+                if (it.isSuccessful){
+                  sendEmailVerification()
+                    startActivity(Intent(this, UserProfile_Activity::class.java))
+                    finish()
+                }else{
+                    Toast.makeText(this ,"failed" + it.exception,Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+
         }
     }
+    private fun sendEmailVerification(){
+        val firebaseUser: FirebaseUser? = auth.currentUser
+        firebaseUser.let {
+            it?.sendEmailVerification()?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext,"email sent to ${txtemail.text.toString()}",Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        val user: FirebaseUser? = auth.currentUser
+        Log.e("user", auth.currentUser.toString())
+        user?.let {
+            startActivity(Intent(this, UserProfile_Activity::class.java))
+            Toast.makeText(applicationContext , "welcome back",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
--- app/src/main/java/com/example/solutionchallenge/UserProfile_Activity.kt
+++ app/src/main/java/com/example/solutionchallenge/UserProfile_Activity.kt
@@ -1,16 +1,24 @@
 package com.example.solutionchallenge
 
 import android.Manifest
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class UserProfile_Activity : AppCompatActivity() {
@@ -18,13 +26,23 @@ class UserProfile_Activity : AppCompatActivity() {
     lateinit var image_profile :ImageView
     private val pickImage = 100
     private var imageUri: Uri? = null
+    private lateinit var database: DatabaseReference
+
+    lateinit var txtage:TextView
+    lateinit var txttall:TextView
+    lateinit var txtweight:TextView
+    lateinit var txtaccount:TextView
+    lateinit var txtage_edit:TextView
+    lateinit var txttall_edit:TextView
+    lateinit var txtweight_edit:TextView
+    lateinit var txtaccount_edit:TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
-        btn_camera = findViewById(R.id.floating_camera_btn)
-        image_profile = findViewById(R.id.image_profile)
+
+        initalize()
 
         btn_camera.setOnClickListener{
 
@@ -42,10 +60,55 @@ class UserProfile_Activity : AppCompatActivity() {
             }
 
         }
+        set_dialog(txtage.text.toString() , txtage_edit)
+        set_dialog(txttall.text.toString() , txttall_edit)
+        set_dialog(txtweight.text.toString() , txtweight_edit)
+        set_dialog(txtaccount.text.toString() , txtaccount_edit)
+
 
 
 
     }
+
+    fun initalize(){
+        btn_camera = findViewById(R.id.floating_camera_btn)
+        image_profile = findViewById(R.id.image_profile)
+
+        txtage = findViewById(R.id.textview_age)
+        txttall = findViewById(R.id.textview_tall)
+        txtweight = findViewById(R.id.textview_weight)
+        txtaccount = findViewById(R.id.textview_account)
+
+        txtage_edit = findViewById(R.id.textview_age_edit)
+        txtweight_edit = findViewById(R.id.textview_weight_edit)
+        txttall_edit = findViewById(R.id.textview_tall_edit)
+        txtaccount_edit = findViewById(R.id.textview_account_edit)
+
+        database = FirebaseDatabase.getInstance().reference
+        database.child("users").child("null").child("email").setValue(imageUri)
+
+    }
+
+    fun set_dialog( title:String , data :TextView){
+
+        data.setOnClickListener{
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setTitle(title)
+            val customlayout:View = layoutInflater.inflate(R.layout.custom_dialog,null)
+            builder.setView(customlayout)
+            builder.setPositiveButton("OK" ,  object :DialogInterface.OnClickListener{
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    var edittext:EditText = customlayout.findViewById(R.id.editText)
+                    data.text = edittext.text.toString()
+                }
+
+            })
+            val dialog = builder.create()
+            dialog.show()
+        }
+
+    }
+
     private fun chooseImageGallery() {
         val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
         startActivityForResult(gallery, pickImage)
@@ -75,7 +138,12 @@ class UserProfile_Activity : AppCompatActivity() {
         if (resultCode == RESULT_OK && requestCode == pickImage) {
             imageUri = data?.data
             image_profile.setImageURI(imageUri)
+            Log.e("image" , imageUri.toString())
+
         }
 
+
     }
+
+
 }
